adb73502cf3c5c35a3b5fbd39f1d12163d5bd5ce
duncanwalker <duncfw@gmail.com>
2013-08-06 23:36:32 +0100

    Set manifest to run the new CreateTrip Activity and changed duration control from number picker to SeekBar to improve compatibility with old Android APIs

--- bikelock/src/main/java/com/understandinggeek/bikelock/CreateTripActivity.java
+++ bikelock/src/main/java/com/understandinggeek/bikelock/CreateTripActivity.java
@@ -1,11 +1,15 @@
 package com.understandinggeek.bikelock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
+import android.widget.SeekBar;
+
+import android.widget.TextView; 
 
 import static java.lang.Long.valueOf;
 
@@ -14,9 +18,38 @@ public class CreateTripActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       Button startButton = (Button) findViewById(R.id.startButton);
-        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.tripDuration);
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.create_trip);
+  	
+        Button startButton = (Button) findViewById(R.id.startButton);
+     
+        SeekBar tripDuration = (SeekBar) findViewById(R.id.tripDurationSeek); 
+    
+        startButton.setOnClickListener(new StartTripClickListener(valueOf(tripDuration.getProgress())));
 
-        startButton.setOnClickListener(new StartTripClickListener(valueOf(numberPicker.getValue())));
+    
+        final TextView tripDurationValue = (TextView)findViewById(R.id.tripDurationText); 
+
+        tripDuration.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){ 
+    		@Override 
+    		public void onProgressChanged(SeekBar seekBar, int progress, 
+    		  boolean fromUser) { 
+    		 // TODO Auto-generated method stub 
+    			tripDurationValue.setText(String.valueOf(progress)); 
+    		} 
+    		
+    		@Override 
+    		public void onStartTrackingTouch(SeekBar seekBar) { 
+    		 // TODO Auto-generated method stub 
+    		} 
+    		
+    		@Override 
+    		public void onStopTrackingTouch(SeekBar seekBar) { 
+    		 // TODO Auto-generated method stub 
+    		} 
+        });  
+
+
+    
     }
 }
